445507bb687f529dbc2a931ab5a3fca3f7b2c25a
chuemoua3 <chuemoua12@outlook.com>
2020-08-20 14:12:25 -0400

    comment out answer

--- src/main/java/com/hifive/songgame/model/Answer.java
+++ src/main/java/com/hifive/songgame/model/Answer.java
@@ -1,28 +1,28 @@
-package com.hifive.songgame.model;
+// package com.hifive.songgame.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
 
-@Entity
-public class Answer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private String answers;
+// @Entity
+// public class Answer {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.AUTO)
+//     private String answers;
 
-    public Answer(){
+//     public Answer(){
         
-    }
+//     }
 
-    public String getAnswers() {
-        return answers;
-    }
+//     public String getAnswers() {
+//         return answers;
+//     }
 
-    public void setAnswers(String answers) {
-        this.answers = answers;
-    }
+//     public void setAnswers(String answers) {
+//         this.answers = answers;
+//     }
 
     
 
-}
+// }
--- src/main/java/com/hifive/songgame/repository/AnswerRepository.java
+++ src/main/java/com/hifive/songgame/repository/AnswerRepository.java
@@ -1,9 +1,9 @@
-package com.hifive.songgame.repository;
+// package com.hifive.songgame.repository;
 
-import com.hifive.songgame.model.Song;
+// import com.hifive.songgame.model.Song;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnswerRepository extends JpaRepository <Song, Long>{
+// public interface AnswerRepository extends JpaRepository <Song, Long>{
     
-}
+// }
